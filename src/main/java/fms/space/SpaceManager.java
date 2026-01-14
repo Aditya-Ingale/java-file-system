b79aea80721f9dd2071ebadbd158c8e03345c0e5
Aditya-Ingale <adityaa.ingale@gmail.com>
2026-01-14 16:08:38 +0530

    add fields to SpaceManager

---
+++ src/main/java/fms/space/SpaceManager.java
@@ -0,0 +1,10 @@
+package fms.space;
+
+import java.util.Queue;
+
+public class SpaceManager{
+    //fields only
+
+    //stores IDs of free block for O(1) allocation
+    private Queue<Interger> freeBlockIds;
+}
