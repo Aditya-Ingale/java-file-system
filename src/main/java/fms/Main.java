efdf3c14fff69140072b0fd8a0f3653e64604dba
Aditya-Ingale <adityaa.ingale@gmail.com>
2026-01-16 00:57:47 +0530

    feat: add CLI entry point

---
+++ src/main/java/fms/Main.java
@@ -0,0 +1,12 @@
+package fms;
+
+import fms.filesystem.FileSystem;'
+
+public class Main {
+    
+    public static void main(String[] args){
+        FileSystem fs = new FileSystem();
+        System.out.println("File System started. Type command.");
+    }
+
+}
