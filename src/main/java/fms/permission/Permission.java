c17e2e11fefb49a56c43e90916aabe27b378d26f
Aditya-Ingale <adityaa.ingale@gmail.com>
2026-01-15 16:57:18 +0530

    feat: add constructor and accessors to permission

--- src/main/java/fms/permission/Permission.java
+++ src/main/java/fms/permission/Permission.java
@@ -5,4 +5,18 @@ public class Permission {
     private boolean canRead;
     private boolean canWrite;
     private boolean canExecute;
+
+    public Permission(boolean canRead, boolean canWrite, boolean canExecute){
+        this.canRead = canRead;
+        this.canWrite = canWrite;
+        this.canExecute = canExecute;
+    }
+
+    public boolean canWrite(){
+        return canWrite;
+    }
+
+    public boolean canRead(){
+        return canRead;
+    }
 }
