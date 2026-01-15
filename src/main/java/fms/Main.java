cbe0c674e0c3c5063a8941569b61bb5c36dec30b
Aditya-Ingale <adityaa.ingale@gmail.com>
2026-01-16 01:04:49 +0530

    feat: add CLI input loop

--- src/main/java/fms/Main.java
+++ src/main/java/fms/Main.java
@@ -1,12 +1,31 @@
 package fms;
 
-import fms.filesystem.FileSystem;'
+import java.util.Scanner;
+
+import fms.filesystem.FileSystem;
 
 public class Main {
     
     public static void main(String[] args){
+
         FileSystem fs = new FileSystem();
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("File System started. Type command.");
+
+        while(true){
+            System.out.print("> ");
+            String input = scanner.nextLine();
+
+            if(input.equalsIgnoreCase("exit")){
+                System.out.println("Exiting file system.");
+                break;
+            }
+
+            System.out.println("Command received: " + input);
+        }
+        
+        scanner.close();
     }
 
 }
